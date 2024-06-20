31a13d6d3d65f339c9af341d68129bac150e2604
unknown <bishalkandi859494@gmail.com>
2024-06-20 19:27:18 +0530

    comment change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodeResolve({
-      browser: true,
-      preferBuiltins: false,
-    }),
-    commonjs(),
-  ],
-  optimizeDeps: {
-    include: ['swiper/react'],
-  },
-  resolve: {
-    alias: {
-      'swiper/react': 'swiper/react/swiper-react.esm.js',
-    },
-  },
-});
-
+  plugins: [react()],
+})
