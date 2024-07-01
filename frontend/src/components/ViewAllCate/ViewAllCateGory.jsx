cf0b0998217da95d1ed28ca704326285da8a17bb
bishal <152034558+bisxxal@users.noreply.github.com>
2024-07-01 09:07:08 +0530

    Update ViewAllCateGory.jsx

--- frontend/src/components/ViewAllCate/ViewAllCateGory.jsx
+++ frontend/src/components/ViewAllCate/ViewAllCateGory.jsx
@@ -4,10 +4,10 @@ import { useNavigate } from 'react-router-dom'
 function ViewAllCateGory() {
     const navigater= useNavigate()
   return (
-    <div className='group relative w-full h-[250px] lg:h-[350px] px-4 lg:px-0 rounded-[60px] overflow-hidden bg-zinc-750 my-16 text-[#ffffff72]'>
+    <div className='group relative w-full h-[250px] lg:h-[350px] px-4 lg:px-0 rounded-[60px] overflow-hidden bg-zinc-750 my-16 text-[#ffffff9c]'>
     <h1
     onClick={() => navigater('/category')} 
-     className=' hidden group-hover:block text-[40px] mx-auto lg:text-[150px] z-[1] top-[20%] left-[10%] font-bold absolute'>VIEW ALL PAIRS &gt; </h1>
+     className=' max-md:text-[40px] text-[150px] z-[1] left-10 top-1/4 max-lg:top-[30%] max-lg:left-[20%] font-bold absolute'>VIEW ALL PAIRS &gt; </h1>
     <img 
       onClick={() => navigater('/category')} 
       className='w-full h-full object-cover rounded-[60px] group-hover:blur-[10px] transition-all duration-300 ease-in-out' 
